bfdac78d30d2ae3fa9cb6725af87a3ebda87e225
Hongzheng Wang <wanghz@gmail.com>
2017-03-22 20:08:50 -0700

    Forgot to submit init-git.el

--- init.el
+++ init.el
@@ -1,8 +1,3 @@
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
 (package-initialize)
 
 (when (version<= emacs-version "24")
@@ -79,6 +74,7 @@
 ;;; Git
 (require 'init-git)
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -86,7 +82,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (magit projectile ag ggtags go-mode cider clojure-mode whole-line-or-region solarized-theme zenburn-theme exec-path-from-shell fullframe))))
+    (magit zenburn-theme whole-line-or-region solarized-theme projectile go-mode ggtags fullframe exec-path-from-shell cider ag))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ lisp/init-git.el
@@ -0,0 +1,6 @@
+(require-package 'magit)
+
+(global-set-key (kbd "C-x g") 'magit-status)
+
+
+(provide 'init-git)
