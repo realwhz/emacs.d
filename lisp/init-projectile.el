0b50e1056a7f84cfb23585603ca9838c999e5526
Hongzheng Wang <hwang@crabel.com>
2018-03-05 11:11:03 -0500

    work around projectile-ag ready-only error

--- lisp/init-projectile.el
+++ lisp/init-projectile.el
@@ -1,5 +1,11 @@
 (require-package 'projectile)
 
+(defun schnouki/projectile-ag (orig-fun &rest args)
+  (let ((grep-find-ignored-directories (copy-sequence grep-find-ignored-directories)))
+    (apply orig-fun args)))
+(advice-add 'projectile-ag :around #'schnouki/projectile-ag)
+;; (advice-remove 'projectile-ag #'schnouki/projectile-ag)
+
 (projectile-mode t)
 
 (provide 'init-projectile)
